7dae29b73e30d66421018d2cafb9061dfdd9993d
pinhomariana <marianapinho1109@gmail.com>
2024-06-23 13:32:42 +0100

    Created Play Your Cards Right

---
+++ javascript.js
@@ -0,0 +1,39 @@
+
+
+let deck = Math.floor(Math.random() * 14) + 1;
+let currentCard = deck
+let CurrentCardText = document.querySelector('p').innerHTML = `The current card is: ${currentCard}.`
+let score = 0
+
+
+function checkGuessHigher() {
+    let next_card = Math.floor(Math.random() * 14) + 1;
+    if (next_card > currentCard) {
+        currentCard = next_card
+        score += 1 
+        console.log("uhu")
+        CurrentCardText = document.querySelector('p').innerHTML = `The current card is: ${currentCard}.`
+    }
+    else {
+        let gameOver = `Wrong Guess. Game Over! Your Score was ${score}!`
+        alert(gameOver)
+        window.location.href = "main.html";
+    }
+}
+
+function checkGuessLower() {
+    let next_card = Math.floor(Math.random() * 14) + 1;
+    if (next_card < currentCard) {
+        currentCard = next_card
+        score += 1 
+        console.log("uhu")
+        CurrentCardText = document.querySelector('p').innerHTML = `The current card is: ${currentCard}.`
+    }
+    else {
+        let gameOver = `Wrong Guess. Game Over! Your Score was ${score}!`
+        alert(gameOver)
+        window.location.href = "main.html";
+    }
+}
+
+
